37c5cdbb9a64ba795da50c5724b483e33c01f32d
gehfand729 <gehfand729@gmail.com>
2022-04-14 19:08:01 +0900

    movement

---
+++ FindingAlice/Assets/_Scripts/Joystick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IBeginDragHandler,IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData){
+        Debug.Log("드래그 시작");
+    }
+    public void OnDrag(PointerEventData eventData){
+        Debug.Log("드래그 중");
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+        Debug.Log("드래그 종료");
+    }
+}
---
+++ FindingAlice/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    private float moveDirX;
+    private Vector3 inputDir;
+    
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpForce;
+
+    private Vector3 j;
+    private GameObject player;
+
+    [SerializeField]
+    private bool isGround;
+
+    private Rigidbody playerRigidbody;
+    private void Start(){
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRigidbody = player.GetComponent<Rigidbody>();
+        j = new Vector3(0,jumpForce,0);
+    }
+    private void Update() {
+        Move();
+        Jump();
+    }
+
+    private void Move(){
+        moveDirX = Input.GetAxisRaw("Horizontal");
+        inputDir = new Vector3(moveDirX,0,0).normalized;
+
+        transform.position += inputDir * speed * Time.deltaTime;
+        if(moveDirX == 0)
+            return;
+        transform.rotation = Quaternion.LookRotation(new Vector3(0,0,moveDirX));
+    }
+    
+    private void Jump(){
+        if(Input.GetKeyDown(KeyCode.Space)&&isGround){
+            playerRigidbody.velocity = transform.up * jumpForce;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Platform"&&other.contacts[0].normal.y>0.7f){
+            isGround = true;
+        }
+    }
+     private void OnCollisionExit(Collision other) {
+        if(other.gameObject.tag == "Platform"){
+            isGround =false;
+        }
+    }
+}
